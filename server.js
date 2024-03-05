09cafae5582d42e812de489b0107346c0c0ab813
Kamil <117399144+Cr3ativeCod3r@users.noreply.github.com>
2024-03-05 21:26:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mariadb = require("mariadb");
 const path = require("path");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000 ;
 const cors = require("cors");
 const session = require("express-session");
 
